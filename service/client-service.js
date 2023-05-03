6f260c3bb91905b76a77f4f60e6b8474311f3ef5
Jorge Lazzarini <tersites.03@gmail.com>
2023-05-03 15:37:27 -0300

    cuesta muchisimo

--- service/client-service.js
+++ service/client-service.js
@@ -1,54 +1,5 @@
-//backticks
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("tr");
-  const contenido = `
-    <td class="td" data-td>
-      ${nombre}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido;
-  return linea;
-};
-
-const table = document.querySelector("[data-table]");
-
-//Abrir http (método,url)
-
-// CRUD   - Métodos HTTP
-// Create - POST
-// Read   - GET
-// Update - PUT/PATCH
-// Delete - DELETE
-// fetch API.
-
 const listaClientes = () =>fetch(" http://localhost:3000/perfil").then(repuesta => respuesta.json());
 
-listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => alert("Ocurrió un error"));
-
-// console.log(data);
-//
+export const clientServices = {
+  listaClientes
+}
