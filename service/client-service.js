9db2b23dd3daf065773a0047727ad5e375420ff1
Jorge Lazzarini <tersites.03@gmail.com>
2023-05-14 20:07:20 -0300

    Agregando clientes/proxima clase eliminando clientes

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,19 @@
 const listaClientes = () =>
-fetch(" http://localhost:3000/perfil").then(repuesta => respuesta.json());
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+
+const crearCliente= (nombre, email) => {
+
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST", 
+    headers : {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({nombre,email, id: uuid.v4()})
+  })
+};
 
 export const clientServices = {
   listaClientes,
+  crearCliente,
 };
